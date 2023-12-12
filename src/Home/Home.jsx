1d6667dcd69ba684202cae2515edd75150730107
saifulemon <463saiful0@gmail.com>
2023-12-12 11:56:02 +0600

    Product showcase displayed and filtered category in home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
+import ProductShocase from './ProductShowcase'
 
 const Home = () => {
   return (
     <>
       <Banner />
       <HomeCategory />
+      <ProductShocase />
     </>
   )
 }
