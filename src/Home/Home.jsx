78229fd96bdd17f84bc1d2ec1264342b0dd809c4
saifulemon <463saiful0@gmail.com>
2023-12-12 12:59:53 +0600

    workshop register section added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
 import ProductShocase from './ProductShowcase'
+import Register from './Register'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <HomeCategory />
       <ProductShocase />
+      <Register />
     </>
   )
 }
---
+++ src/Home/Register.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+const subtitle = "Save the Day";
+const title = (
+  <h2 className="title">
+    Join on Day Long free workshop for{" "}
+    <b>
+      Advance <span>Mastering</span>
+    </b>
+    on Sales
+  </h2>
+);
+const desc = "Limited Time Offer! Hurry up the day";
+
+const Register = () => {
+  return (
+    <section className="register-section padding-tb pb-0">
+      <div className="container">
+        <div className="row g-4 row-cols-lg-2 row-cols-1 align-items-center">
+          <div className="col">
+            <div className="section-header">
+              <span className="subtitle">{subtitle}</span>
+              {title}
+              <p>{desc}</p>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="section-wrapper">
+              <h4>Register Now</h4>
+              <form className="register-form">
+                <input type="text" name="name" placeholder="Username" className="reg-input"/>
+                <input type="email" name="email" placeholder="Email" className="reg-input"/>
+                <input type="number" name="number" placeholder="Phone" className="reg-input"/>
+                <button type="submit" className="lab-btn">
+                  Register Now
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
