b3b892aab21250247eb1c320d269e6025c469661
saifulemon <463saiful0@gmail.com>
2023-12-12 14:18:24 +0600

    location spread section completed

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner'
 import HomeCategory from './HomeCategory'
 import ProductShocase from './ProductShowcase'
 import Register from './Register'
+import LocationSpread from './LocationSpread'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <HomeCategory />
       <ProductShocase />
       <Register />
+      <LocationSpread />
     </>
   )
 }
